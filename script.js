30d86cf4ba8030f768d59b5ed3e8c3602536d121
Nicolas <nicolaxortiz@DESKTOP-B8NBE5T.localdomain>
2022-09-14 20:38:59 -0500

    pagina finalizada

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnLogin = document.querySelector(".btnLogin");
+const formLogin = document.querySelector(".formLogin");
+
+btnLogin.addEventListener("click", () => {
+    formLogin.classList.toggle("borrar")
+})
